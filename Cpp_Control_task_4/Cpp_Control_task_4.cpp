6b8f5e48925151f36fb6883cda15c61138220806
juraGR2025 <93720304+juraGR2025@users.noreply.github.com>
2022-06-26 19:43:46 +0300

    Add files via upload

--- Cpp_Control_task_4/Cpp_Control_task_4.cpp
+++ Cpp_Control_task_4/Cpp_Control_task_4.cpp
@@ -20,7 +20,6 @@ int Myroot(double a, double b, double c, double& x1, double& x2)
 {
     double D = b * b - 4.0 * a * c;
     if (D < 0) {
-        cout << "Корней нет" << endl;
         return -1;
     }
     if (a == 0)
@@ -28,19 +27,17 @@ int Myroot(double a, double b, double c, double& x1, double& x2)
         if (b == 0)
             return -1;
         x2 = x1 = -c / b;
-        cout << "x1=x2=" << x1 << endl;
         return 0;
     }
     if (D == 0)
     {
         x1 = -b / (2.0 * a);
         x2 = x1;
-        cout << "x1=x2=" << x1 << endl;
+
         return 0;
     }
     x1 = (-b + sqrt(D)) / (2.0 * a);
     x2 = (-b - sqrt(D)) / (2.0 * a);
-    cout << "x1=" << x1 << "\t" << "x2=" << x2 << endl;
     return 1;
 }
 /*Требуется реализовать функцию Input(a,b). 
@@ -58,12 +55,18 @@ bool Input(int a, int b) {
 int main()
 {
     setlocale(0, "");
-    double a, b, c, x1, x2;
+    double a, b, c, x1=0, x2=0;
+    int flag;
     cout << "a="; cin >> a;
     cout << "b="; cin >> b;
     cout << "c="; cin >> c;
-    Myroot(a, b, c, x1, x2);
-
+    flag = Myroot(a, b, c, x1, x2);
+    cout << "Flag = " << flag << endl;
+    if (flag == 1) {
+        cout << "x1=" << x1 << "\t" << "x2=" << x2 << endl;
+    }
+    else if(flag == 0)cout << "x1=x2=" << x1 << endl;
+    else  cout << "Вещественных корней нет " << endl;
     int a1, b1;
     a1 = 0;
     b1 = 0;
